92ca3c08d6dc639d5a09aece8312035b03b2fe17
Erick Lara <ericklarita@gmail.com>
2018-04-27 13:35:40 -0500

    Added numeral on cell.

--- components/cell.js
+++ components/cell.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react';
 import { View, ViewPropTypes, Text, StyleSheet } from 'react-native';
 
+const formatPattern2 = '0,0';
+const formatPattern = '0,0.00';
+
 class Cell extends Component {
   static propTypes = {
     style: ViewPropTypes.style,
     textStyle: Text.propTypes.style,
     borderStyle: ViewPropTypes.style,
-  }
+  };
+
+
+    _getFormattedValue = function (value) {
+        return value%1 == 0
+            ? Numeral(value).format(formatPattern2)
+            : Numeral(Number(value.toFixed(2))).format(formatPattern);
+    };
 
   render() {
     const {data, width, height, flex, style, textStyle} = this.props;
     const textDom = React.isValidElement(data) ? data : (
-        <Text style={[textStyle, styles.text]}>{typeof data == "number" ? data.toLocaleString() : data}</Text>
+        <Text style={[textStyle, styles.text]}>{typeof data == "number" ? this._getFormattedValue(data) : data}</Text>
       );
     let borderWidth,borderColor;
     if (this.props.borderStyle && this.props.borderStyle.borderWidth !== undefined) {
